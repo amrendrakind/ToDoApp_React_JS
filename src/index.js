60ad0da76b9a7c395f49f9f9d801ef7b39a510cb
amrendrakind <vision2010@gmail.com>
2022-05-05 00:09:03 +0530

    Updated App.css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import TodoContainer from './components/TodoContainer';
 import reportWebVitals from './reportWebVitals';
+import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
