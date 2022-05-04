f513006fea6a7c603a7429d39f8f611687fac4d9
amrendrakind <vision2010@gmail.com>
2022-05-04 23:03:42 +0530

    Add handel Submit functionality

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,11 +1,31 @@
 import React, { PureComponent } from 'react';
 
 class InputTodo extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+    };
+  }
+
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { title } = this.state;
+    console.log(title);
+  };
+
   render() {
+    const { title } = this.state;
     return (
-      <form>
-        <input type="text" placeholder="Add Todo..." />
-        <button type="button">Submit</button>
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Add Todo..." value={title} name="title" onChange={this.onChange} />
+        <button type="submit">Submit</button>
       </form>
     );
   }
