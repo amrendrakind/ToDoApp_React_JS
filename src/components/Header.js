5de7cc8361d662d40146da817ff155b9274c416b
Amrendra K <amrendrak.ind@gmail.com>
2023-06-01 18:31:09 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,9 @@
 import React from 'react';
+import './Header.css'; // Assuming you have a separate CSS file for header styles
 
-const headerStyle = {
-  padding: '20px 0',
-  lineHeight: '1.5em',
-};
 const Header = () => (
-  <header style={headerStyle}>
-    <h1
-      style={{
-        fontSize: '6rem',
-        fontWeight: '600',
-        marginBottom: '2rem',
-        lineHeight: '1em',
-        color: '#ececec',
-        textTransform: 'lowercase',
-        textAlign: 'center',
-      }}
-    >
+  <header className="header-style">
+    <h1 className="header-title">
       todos:
     </h1>
   </header>
