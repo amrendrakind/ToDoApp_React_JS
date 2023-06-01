0c6cc611377c5a1a249e8c79b37bc5b5f1582852
Amrendra K <amrendrak.ind@gmail.com>
2023-06-01 18:29:38 +0100

    Add descriptive test name

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('should render the "Learn React" link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
